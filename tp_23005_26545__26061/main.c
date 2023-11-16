fb964d99107095687bbea4e52064cc27e4e632c7
Queiros028 <wadosecsgo@gmail.com>
2023-11-16 17:21:02 +0000

    16-11

---
+++ tp_23005_26545__26061/main.c
@@ -0,0 +1,72 @@
+/*****************************************************************//**
+ * \file   main.c
+ * 
+ * \author Eduardo Queirós, João Lima, Luís Gonçalves
+ * \date   November 2023
+ *********************************************************************/
+#include<stdio.h>
+
+/**
+ *
+ * \function name- menuPrincipal
+ * \brief- Esta função será utilizada como um menu principal do programa. Este menu será apresentado no início do programa. 
+ *		   A função irá perguntar ao jogador qual o que ele quer fazer no jogo. 
+ *  
+ */
+int mainMenu() 
+{
+	int opcaoPrincipal;
+	printf("Welcome to the game of the Rings\n");
+	printf("1- Start new game\n");
+	printf("2- Load game\n");
+	printf("3- Settings\n");
+	printf("0- Exit\n");
+	printf("Option: \n");
+	printf("\n");
+	scanf_s("%d", &opcaoPrincipal);
+	return(opcaoPrincipal);
+
+}
+
+
+int StartNewGameMenu() {
+
+}
+
+int LoadGameMenu() {
+
+}
+
+int SettingsMenu() {
+
+}
+
+
+int NewGame() {
+
+}
+
+int LoadGame() {
+
+}
+
+int Settings() {
+
+}
+
+int main()
+{
+	int opPrincipal =  mainMenu();	
+	switch (opPrincipal)
+	{
+	case 1: NewGame();
+		break;
+	case 2: LoadGame();
+		break;
+	case 3: Settings();
+		break;
+	default:printf("Invalid Option!!!!");
+		break;
+	}
+
+}
